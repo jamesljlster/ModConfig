344d030cd4e24cbd1b9d82d13327768bab9a55ab
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-11-02 20:25:23 +0800

    Remove stable test

--- test/simple_test.c
+++ test/simple_test.c
@@ -35,17 +35,6 @@ int main()
 //	}
 //	free(strList);
 
-	while(1)
-	{
-		iResult = modcfg_create(&mod, TEST_PATH);
-		if(iResult != MODCFG_NO_ERROR)
-		{
-			printf("modcfg_create() failed with error: %d\n", iResult);
-			return -1;
-		}
-		modcfg_delete(mod);
-	}
-	
 	iResult = modcfg_create(&mod, TEST_PATH);
 	if(iResult != MODCFG_NO_ERROR)
 	{
