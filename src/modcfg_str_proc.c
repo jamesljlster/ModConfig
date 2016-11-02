eeb19965833684b9ee34951d2db7bdae4bba3a85
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-11-02 18:41:55 +0800

    Add append character

---
+++ src/modcfg_str_proc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "ModConfig.h"
+
+int modcfg_srt_append(char** strBufPtr, int* strBufLenPtr, char appendChar)
+{
+	int tmpStrBufLen = *strBufLenPtr;
+	char* tmpStrBuf = *strBufPtr;
+	void* allocTmp = NULL;
+
+	// Append character to string buffer
+	tmpStrBufLen++;
+	allocTmp = realloc(tmpStrBuf, sizeof(char) * tmpStrBufLen);
+	if(allocTmp == NULL)
+	{
+		return MODCFG_MEM_FAILED;
+	}
+	else
+	{
+		tmpStrBuf = (char*)allocTmp;
+		tmpStrBuf[tmpStrBufLen - 1] = '\0';
+		tmpStrBuf[tmpStrBufLen - 2] = appendChar;
+	}
+
+	// Assign Value
+	*strBufPtr = tmpStrBuf;
+	*strBufLenPtr = tmpStrBufLen;
+
+	return MODCFG_NO_ERROR;
+}
