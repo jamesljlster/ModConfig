cbdcce512ec0a3ff5c4f57da3521d0f32565e1f5
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-11-02 20:33:23 +0800

    Add stable test (standalone)

---
+++ test/stable_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <ModConfig.h>
+
+#define TEST_PATH	"./dev.txt"
+
+int main()
+{
+	int iResult;
+	MODCFG mod = NULL;
+	
+	while(1)
+	{
+		iResult = modcfg_create(&mod, TEST_PATH);
+		if(iResult != MODCFG_NO_ERROR)
+		{
+			printf("modcfg_create() failed with error: %d\n", iResult);
+			return -1;
+		}
+
+		modcfg_delete(mod);
+	}
+
+	return 0;
+}
