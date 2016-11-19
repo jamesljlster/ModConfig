b7604aa789e322f83a9dfaadceeee9bc4ad4c438
Zheng-Ling Lai <jamesljlster@gmail.com>
2016-11-19 18:23:53 +0800

    Remove CMakeLists.txt in install file list

--- src/modcfg_merge.c
+++ src/modcfg_merge.c
@@ -118,5 +118,98 @@ RET:
 	return retValue;
 }
 
-int modcfg_merge_struct(struct MODCFG_STRUCT* dst, struct MODCFG_STRUCT* src);
+int modcfg_merge_struct(struct MODCFG_STRUCT* dst, struct MODCFG_STRUCT* src)
+{
+	int i;
+	int iResult;
+	int retValue;
+	
+	int cmpLen;
+	int srcLen, dstLen;
+
+	struct MODCFG_MODULE* ptr = NULL;
+	void* allocTmp = NULL;
+
+	for(i = 0; i < src->modCount; i++)
+	{
+		// Try to find same struct
+		ptr = modcfg_search_module(dst, src->modList[i].modName);
+		if(ptr == NULL)
+		{
+			// Insert new module
+			// Memory allocation: New space for new member
+			allocTmp = realloc(dst->modList, sizeof(struct MODCFG_MODULE) * (dst->modCount + 1));
+			if(allocTmp == NULL)
+			{
+				retValue = MODCFG_MEM_FAILED;
+				goto RET;
+			}
+			else
+			{
+				dst->modCount++;
+				dst->modList = (struct MODCFG_MODULE*)allocTmp;
+				allocTmp = NULL;
+
+				dst->modList[dst->modCount - 1].modName = NULL;
+				dst->modList[dst->modCount - 1].modType = NULL;
+				dst->modList[dst->modCount - 1].memberCount = NULL;
+				dst->modList[dst->modCount - 1].memberList = NULL;
+			}
+
+			// Clone new member to new space
+			iResult = modcfg_clone_module(&dst->modList[dst->modCount - 1], &src->modList[i]);
+			if(iResult != MODCFG_NO_ERROR)
+			{
+				retValue = iResult;
+				goto RET;
+			}
+		}
+		else
+		{
+			// Check if module type string is empty
+			if(ptr->modType == NULL)
+			{
+				// Copy content
+				ptr->modType = modcfg_str_clone(src->modList[i].modType);
+				if(ptr->modType == NULL)
+				{
+					retValue = MODCFG_MEM_FAILED;
+					goto RET;
+				}
+			}
+			else
+			{
+				// Checking if type string is same
+				dstLen = strlen(ptr->modType);
+				srcLen = strlen(src->modList[i].modType);
+				if(dstLen != srcLen)
+				{
+					retValue = MODCFG_CONFLICT;
+					goto RET;
+				}
+				
+				iResult = strncmp(ptr->modType, src->modList[i].modType, cmpLen);
+				if(iResult != 0)
+				{
+					retValue = MODCFG_CONFLICT;
+					goto RET;
+				}
+			}
+
+			// Merge module
+			iResult = modcfg_merge_module(ptr, &src->modList[i]);
+			if(iResult != MODCFG_NO_ERROR);
+			{
+				retValue = iResult;
+				goto RET;
+			}
+		}	
+	}
+
+RET:
+	if(allocTmp != NULL)
+		free(allocTmp);
+
+	return retValue;
+}
 
